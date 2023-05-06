ee2e5812004a238dd727d7a9d5bcaef945d40514
thorstenfr <tfreimann@gmail.com>
2023-05-06 22:00:07 +0200

    Details können angezeigt werden

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+/* 
+ * Deaktivieren während Entwicklung
+
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('sw.js')
     .then((registration) => {
@@ -8,9 +12,16 @@ if ('serviceWorker' in navigator) {
     });
 }
 
+*/
+
+
 const counterElement = document.getElementById('counter');
 const incrementButton = document.getElementById('increment');
 const decrementButton = document.getElementById('decrement'); // Neuer Button
+const showDetailsButton = document.getElementById('show-details');
+
+const counterArray = [];
+
 
 // Laden des gespeicherten Zählerstands
 const storedCounter = localStorage.getItem('counter');
@@ -22,17 +33,50 @@ incrementButton.addEventListener('click', () => {
   const currentCounter = parseInt(counterElement.textContent, 10);
   const newCounter = currentCounter + 1;
   counterElement.textContent = newCounter;
+  
+  // Create a new counter object with the current count and date
+  const counter = { count: newCounter, date: new Date() };
 
-  // Speichern des neuen Zählerstands
-  localStorage.setItem('counter', newCounter);
+  // Add the new counter object to the counter array
+  counterArray.push(counter);
+
+  // Save the counter array to local storage
+  localStorage.setItem('counterArray', JSON.stringify(counterArray));
 });
+
 
 // Neuer Button-Click-Handler
 decrementButton.addEventListener('click', () => {
   const currentCounter = parseInt(counterElement.textContent, 10);
   const newCounter = Math.max(currentCounter - 1, 0);
   counterElement.textContent = newCounter;
+  
+  // Create a new counter object with the current count and date
+  const counter = { count: newCounter, date: new Date() };
 
-  // Speichern des neuen Zählerstands
-  localStorage.setItem('counter', newCounter);
+  // Add the new counter object to the counter array
+  counterArray.push(counter);
+
+  // Save the counter array to local storage
+  localStorage.setItem('counterArray', JSON.stringify(counterArray));
 });
+
+showDetailsButton.addEventListener('click', () => {
+  const detailsElement = document.getElementById('details');
+  detailsElement.innerHTML = ''; // Clear any existing details
+  
+  // Create an unordered list element to hold the counter details
+  const listElement = document.createElement('ul');
+  
+  // Add each counter object to the list element
+  counterArray.forEach((counter) => {
+    const listItemElement = document.createElement('li');
+    const date = new Date(counter.date);
+    listItemElement.textContent = `Count: ${counter.count}, Date: ${date.toLocaleString()}`;
+    listElement.appendChild(listItemElement);
+  });
+  
+  // Add the list element to the details element
+  detailsElement.appendChild(listElement);
+});
+
